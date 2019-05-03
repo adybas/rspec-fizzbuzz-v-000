2edbd6812dffe3a0a8f3f66880d95bd1eacd6199
Anna Dybas <a.m.dybas@gmail.com>
2019-05-03 22:39:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 def fizzbuzz(number)
 
   if number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+    "Buzz"
   end
 
 end
